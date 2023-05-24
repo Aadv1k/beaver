50c4f8555aafdbd11147a3b4990be2841b7fb97c
aadv1k <aadv1k@outlook.com>
2023-05-24 11:20:41 +0530

    Add melt functionality and other utility

--- main.lisp
+++ main.lisp
@@ -14,7 +14,7 @@
     (push current tokens)
     (reverse tokens)))
 
-(defun read-csv-file (filepath &optional (delem ","))
+(defun read-csv (filepath &optional (delem ","))
   (let ((data '()))
   (with-open-file (file filepath :external-format :iso-8859-1)
     (dotimes (i max-file-len)
@@ -42,14 +42,27 @@
       (let ((row '()))
         (dotimes (j (length (nth i data)))
           (push (string-sanitize (nth j (nth i data))) row))
-        (push (nreverse row) cleaned-data))) ; Reverse the row to maintain the original order
-    (nreverse cleaned-data))) ; Reverse the cleaned-data to maintain the original order
+        (push (nreverse row) cleaned-data)))
+    (nreverse cleaned-data)))
+
+(defun get-col (data name)
+  (let ((idx nil)
+        (head (first data))
+        (result '()))
+    
+    (dotimes (i (length head))
+      (when (string= (nth i head) name)
+        (setq idx i)
+        (return)))
+    
+    (if (not idx) (return-from get-col nil))
+
+    (loop :for i :from 1 :below (length data)
+          :do (push (nth idx (nth i data)) result)
+          )
+    (nreverse result)))
 
 
-;;(defun remove-special-characters (string) (let ((special-chars "!@#$%^&*()_+|{}[];':\",.<>/?\\`~-=")) (replace-regexp-in-string (format "[%s]" special-chars) "" string)))
-
-
-;; expects flat data only
 (defun transpose-row-to-col (data)
   (let ((output '()) (top (nth 0 data)))
     (dotimes (i (length top))
@@ -61,4 +74,27 @@
     (reverse output)))
 
 
-(print (clean-csv (read-csv-file "data.csv" ";")))
+
+(defun melt (data from-cols to-cols)
+  (assert (eq (length to-cols) 2))
+  (assert (> (length from-cols) 1))
+
+  (let ((melted-values '()) (melted-types '()))
+    (dotimes (i (length from-cols))
+      (dolist (elem (reverse (get-col data (nth i from-cols))))
+      (push (nth i from-cols) melted-types)
+        (push elem melted-values)
+        )
+      )
+    (list to-cols melted-values  melted-types)
+    ))
+
+
+(defun print-matrix (matrix)
+(format t "~{~{~a~^ ~}~%~}" matrix)
+  )
+
+
+
+
+(print-matrix (melt (clean-csv (read-csv "data.csv" ";")) '("First name" "Last name" "Username") '("type" "value")))
